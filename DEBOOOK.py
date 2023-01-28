1bbd3d0835a5cb8f2326ab31a918c03263b0b9cd
JuanCarlos200321 <juancarlosplate@ufl.edu>
2023-01-28 16:05:12 -0500

    I LOVE HACKING heheeh

--- DEBOOOK.py
+++ DEBOOOK.py
@@ -1,7 +1,10 @@
 playing = True
 while playing:
-    player = int(input("Your Hands Total: "))
+
+    player = int(input("Your Hands Total (soft or hard)"))
     dealer = int(input("Dealers Face Up: "))
-    if player == 21:
-        print("Blackjack, You Win")
-        break
+    # if player == 21:
+    #     print("Blackjack, You Win")
+    #     break
+
+    if player == (1,2,3,4,5):
