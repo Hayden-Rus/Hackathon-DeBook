17d636f07b9b0a07da4e21efd01ee57fc270453b
JuanCarlos200321 <juancarlosplate@ufl.edu>
2023-01-29 01:56:01 -0500

    Final

--- DEBOOOK.py
+++ DEBOOOK.py
@@ -14,8 +14,7 @@ red_card = pygame.image.load('Player Card.jpg')
 red_card = pygame.transform.scale(red_card,(200, 250))
 blue_card = pygame.image.load('Dealers Card.jpg')
 blue_card = pygame.transform.scale( blue_card,(200, 250))
-double_pic = pygame.image.load('Double.jpg').convert_alpha(screen)
-double_pic = pygame.transform.scale(double_pic,(800, 600))
+
 
 
 
@@ -392,68 +391,10 @@ def draw_debook_opened(win):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-def double_screen(win):
-    start_title_font = pygame.font.Font(None, 75)
-    button_font = pygame.font.Font(None, 50)
-    win.fill((255, 255, 240))
-    win.blit(double_pic, (0, 0))
-    pygame.display.flip()
-
-    title_surface = start_title_font.render("You know you gotta Double it!!", 0, ((255,255,0)))
-    title_rectangle = title_surface.get_rect(
-        center=(WIDTH // 2, HEIGHT // 2 + 80))
-    win.blit(title_surface, title_rectangle)
-
-    exit_text = button_font.render("Return", 0, (255, 255, 255))
-    exit_surface = pygame.Surface((exit_text.get_size()[0] + 20, exit_text.get_size()[1] + 20))
-    exit_surface.fill(LINE_COLOR)
-    exit_surface.blit(exit_text, (10, 10))
-
-    exit_rectangle = exit_surface.get_rect(
-        center=(WIDTH // 2 - 100, HEIGHT // 2 + 270))
-
-    win.blit(exit_surface, exit_rectangle)
-
-    quit_text = button_font.render("Quit", 0, (255, 255, 255))
-    quit_surface = pygame.Surface((quit_text.get_size()[0] + 20, quit_text.get_size()[1] + 20))
-    quit_surface.fill(LINE_COLOR)
-    quit_surface.blit(quit_text, (10, 10))
-
-    quit_rectangle = quit_surface.get_rect(
-        center=(WIDTH // 2 + 100, HEIGHT // 2 + 270))
-    win.blit(quit_surface, quit_rectangle)
-
-    pygame.display.update()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if quit_rectangle.collidepoint(event.pos):
-                    sys.exit()
-                if exit_rectangle.collidepoint(event.pos):
-                   draw_debook_opened(screen)
-
-    pass
-
-
 def main():
     pygame.display.set_caption("Feeling Lucky?")
     draw_debook_cover(screen)
 
 if __name__ == '__main__':
     main()
-    #hehehehe
 
